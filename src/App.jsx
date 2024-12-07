b18a60655893233d146d3a3ad5b6469ed8aefefb
lennyaiko <lennyaiko17@gmail.com>
2024-12-07 21:41:22 +0100

    progress on stuff

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,40 @@ import "./App.css";
 import ClearButton from "./components/ClearButton";
 import Board from "./components/Board";
 import data from "./utils/data";
-import {
-  readText,
-  readImage,
-  clear,
-} from "@tauri-apps/plugin-clipboard-manager";
+import { clear } from "@tauri-apps/plugin-clipboard-manager";
 import useClipboardListener from "./hooks/useClipboardListener";
+import { readFile, writeFile, BaseDirectory } from "@tauri-apps/plugin-fs";
 
 function App() {
   const [items, setItems] = useState([]);
-  const [data, setData] = useState([]);
-  useClipboardListener(1000, setItems);
 
-  useEffect(() => {
-    setData([...data, items]);
-  }, [items]);
+  const data = useClipboardListener(1000, setItems);
 
-  async function greet() {
-    const text = await readText();
-    console.log(text);
+  // useEffect(() => {
+  //   setData([...data, items]);
+  // }, [items]);
+
+  async function clearClipboard() {
+    setData([]);
+    await clear();
   }
+
+  // Read file content
+  const readFile = async () => {
+    try {
+      const content = await readFile("data.json", {
+        dir: BaseDirectory.Home,
+      });
+      setFileContent(content);
+      console.log("File read successfully:", content);
+    } catch (error) {
+      console.error("Error reading file:", error);
+    }
+  };
 
   return (
     <main className="container min-h-screen bg-blue-950">
-      <ClearButton onClick={greet} />
+      <ClearButton onClick={clearClipboard} />
 
       <div className="overflow-hidden h-[500px] overflow-y-scroll">
         <Board items={data} />
--- src/hooks/useClipboardListener.js
+++ src/hooks/useClipboardListener.js
@@ -1,26 +1,26 @@
 import { useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
+import { readText, readImage } from "@tauri-apps/plugin-clipboard-manager";
 import {
-  readText,
-  readImage,
-  clear,
-} from "@tauri-apps/plugin-clipboard-manager";
+  readTextFile,
+  readFile,
+  writeTextFile,
+  BaseDirectory,
+} from "@tauri-apps/plugin-fs";
 
-const useClipboardListener = (pollInterval = 1000, data = []) => {
+const useClipboardListener = (pollInterval = 1000, item = "") => {
   const [clipboardContent, setClipboardContent] = useState("");
+  const [fileContent, setFileContent] = useState("");
+  const [newContent, setNewContent] = useState("");
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    let lastClipboardContent = "";
-
     const checkClipboard = async () => {
       try {
-        // const content = await invoke("plugin:clipboard|read_text");
         const content = await readText();
-        if (content && content !== lastClipboardContent) {
-          lastClipboardContent = content;
-          setClipboardContent(content);
-          data(content);
-          console.log("Clipboard content changed:", content);
+
+        if (content && !data.find((item) => item === content)) {
+          item(content);
+          setData([...data, content]);
         }
       } catch (err) {
         console.error("Error reading clipboard:", err);
@@ -32,7 +32,7 @@ const useClipboardListener = (pollInterval = 1000, data = []) => {
     return () => clearInterval(interval); // Cleanup on unmount
   }, [pollInterval]);
 
-  return clipboardContent;
+  return data;
 };
 
 export default useClipboardListener;
