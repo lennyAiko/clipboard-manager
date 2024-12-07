36f7a7204d50a827b5dbbdd1d8dc8a004d372008
lennyaiko <lennyaiko17@gmail.com>
2024-12-07 18:57:10 +0100

    progress on clipboard management

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ClearButton from "./components/ClearButton";
 import Board from "./components/Board";
 import data from "./utils/data";
 import {
-  clear,
+  writeText,
   readText,
-  readImage,
+  clear,
 } from "@tauri-apps/plugin-clipboard-manager";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
-
   async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    // setGreetMsg(await invoke("greet", { name }));
-    console.log(await readText());
+    console.log("greet");
+    await writeText("Tauri is awesome!");
+    // const text = await readText();
+    // console.log(text);
   }
 
-  console.log(data);
   return (
     <main className="container min-h-screen bg-blue-950">
       <ClearButton onClick={greet} />
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -8,10 +8,11 @@ const Board = (props) => {
   };
   return (
     <div className="flex flex-col justify-center w-full align-center">
-      {items.map((item) => (
+      {items.map((item, index) => (
         <div
           className="cursor-pointer rounded-xl mx-3 my-1.5 p-3 bg-blue-600 text-white h-28 overflow-hidden text-wrap active:scale-95 select-none active:bg-blue-400 duration-200 transition-all"
           onClick={() => clicker(item)}
+          key={index}
         >
           {item}
         </div>
